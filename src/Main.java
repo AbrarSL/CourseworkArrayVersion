708df4ce11f41380958eac9b67088549ed794baf
AbrarSL <abrarsl2002@gmail.com>
2023-06-23 08:50:56 +0530

    Add inline comments and fix minor problems

--- src/Main.java
+++ src/Main.java
@@ -15,10 +15,11 @@ public class Main {
     private static int burgerStock = 50;
 
     public static void main(String[] args) {
-        initQueues();
+        initQueues(); // Queues must always be initialized before doing anything else
         displayCommands();
 
         while (true) {
+            // All commands are converted to uppercase for easier parsing
             String command = inputPrompt("Enter a command (press \"H\" for help): ").strip().toUpperCase();
 
             if (command.equals("EXT") || command.equals("999")) {
@@ -26,6 +27,7 @@ public class Main {
                 break;
             }
 
+            // Option selection
             switch (command) {
                 case "H":
                     displayCommands();
@@ -67,8 +69,7 @@ public class Main {
     }
 
     /**
-     * Initializes the queues.
-     * This must *always* be called at the beginning of the main() method.
+     * Initializes the child queues.
      * This method mutates {@link Main#cashiers}
      */
     private static void initQueues() {
@@ -106,7 +107,7 @@ public class Main {
      * @param headerText The text that will be displayed.
      */
     private static void displayHeader(String headerText) {
-        final int headerLength = headerText.length() + 10;
+        final int headerLength = headerText.length() + 10; // 10 is added to give the header some horizontal padding
         final int titlePaddingLength = (headerLength - 2 - headerText.length()) / 2;
 
         System.out.println("*".repeat(headerLength));
@@ -140,6 +141,7 @@ public class Main {
             try {
                 int selectedNumber = Integer.parseInt(inputPrompt(prompt));
 
+                // Ensure number is within range
                 if (selectedNumber <= rangeEnd && selectedNumber >= rangeStart) {
                     return selectedNumber;
                 }
@@ -160,6 +162,8 @@ public class Main {
      * @return A validated String object.
      */
     private static String validatedInputPrompt(String prompt) {
+        // TODO: Refactor this method to use RegEx
+        // TODO: Add more validation rules
         String[] forbiddenStrings = {"null"};
         String userInput = "";
         boolean notValid = true;
@@ -181,6 +185,7 @@ public class Main {
 
             for (String forbiddenText : forbiddenStrings) {
                 if (userInput.equals(forbiddenText)) {
+                    System.out.println("Invalid text detected!");
                     notValid = true;
                     break;
                 }
@@ -200,7 +205,7 @@ public class Main {
      * @return 'false' if the operation failed (No free space in the array).
      */
     private static boolean addToQueue(String[] queue, String customer) {
-        for (int i = 0; i < queue.length; i++) {
+        for (int i = 0; i < queue.length; i++) { // Linear search for the first null position
             if (queue[i] == null) {
                 queue[i] = customer;
                 return true;
@@ -250,7 +255,7 @@ public class Main {
     private static String[] getLongestQueue(String[][] queues) {
         String[] longest = queues[0];
 
-        for (String[] queue : queues) {
+        for (String[] queue : queues) { // Linear search
             if (queue.length > longest.length) {
                 longest = queue;
             }
@@ -270,14 +275,14 @@ public class Main {
     private static String[] flattenQueues(String[][] queues) {
         int totalLength = 0;
 
-        for (String[] queue : queues) {
+        for (String[] queue : queues) { // Sum all child array sizes
             totalLength += queue.length;
         }
 
         String[] tempQueue = new String[totalLength];
         int queueIndex = 0;
 
-        for (String[] queue : queues) {
+        for (String[] queue : queues) { // Copy all non-null refs
             for (String name : queue) {
                 if (name != null) {
                     tempQueue[queueIndex] = name;
@@ -288,20 +293,20 @@ public class Main {
 
         int validItemsLength = tempQueue.length;
 
-        for (int i = 0; i < tempQueue.length; i++) {
+        for (int i = 0; i < tempQueue.length; i++) { // Find extent of valid elements
             if (tempQueue[i] == null) {
                 validItemsLength = i;
                 break;
             }
         }
 
-        if (validItemsLength == tempQueue.length) {
+        if (validItemsLength == tempQueue.length) { // Avoid copy if all elements were valid
             return tempQueue;
         }
 
-        String[] newQueue = new String[validItemsLength];
+        String[] newQueue = new String[validItemsLength]; // Properly sized array for valid elements
 
-        for (int i = 0; i < newQueue.length; i++) {
+        for (int i = 0; i < newQueue.length; i++) { // Copy element refs to new array
             newQueue[i] = tempQueue[i];
         }
 
@@ -345,12 +350,16 @@ public class Main {
      * @return 'true' if strings should be swapped to maintain alphabetical order.
      */
     private static boolean stringShouldSwap(String firstString, String secondString) {
+        // Lowercase all arguments to ignore case
         firstString = firstString.toLowerCase();
         secondString = secondString.toLowerCase();
+
+        // Use the shortest string to avoid indexing out of bounds
         String shortestString = firstString.length() > secondString.length() ? secondString : firstString;
         boolean isLengthEqual = firstString.length() == secondString.length();
 
-        for (int i = 0; i < shortestString.length(); i++) {
+        for (int i = 0; i < shortestString.length(); i++) { // Compare order of each character pair
+            // ASCII values are checked
             if (firstString.charAt(i) == secondString.charAt(i)) {
                 continue;
             }
@@ -362,7 +371,7 @@ public class Main {
             return false;
         }
 
-        return secondString == shortestString;
+        return secondString == shortestString; // Shorter string is considered alphabetically higher
     }
 
     /**
@@ -373,17 +382,17 @@ public class Main {
      */
     private static void viewAllQueues(String[][] queues) {
         final String titleText = "Cashier";
-        final int longestQueueLength = getLongestQueue(queues).length;
-        final int headerLength = titleText.length() + 10;
+        final int longestQueueLength = getLongestQueue(queues).length; // Ensures that all elements are considered
+        final int headerLength = titleText.length() + 10; // Ensure queue padding matches up with header
         final int paddingLength = ((headerLength / queues.length) - 1) / 2;
 
         displayHeader(titleText);
 
-        for (int i = 0; i < longestQueueLength; i++) {
+        for (int i = 0; i < longestQueueLength; i++) { // Calculate child array index in outer loop
             for (String[] queue : queues) {
                 System.out.print(" ".repeat(paddingLength));
 
-                if (queue.length <= i) {
+                if (queue.length <= i) { // Ensure index is not out of bounds
                     System.out.print(" ");
                 } else if (queue[i] == null) {
                     System.out.print("X");
@@ -408,11 +417,13 @@ public class Main {
     private static void viewEmptyQueues() {
         String[][] tempQueues = new String[3][];
 
-        for (int i = 0; i < cashiers.length; i++) {
-            if (cashiers[i][cashiers[i].length - 1] == null) {
+        for (int i = 0; i < cashiers.length; i++) { // Find all full queues
+            int lastIndex = cashiers[i].length - 1;
+
+            if (cashiers[i][lastIndex] == null) { // Checks if tail of queue is free
                 tempQueues[i] = cashiers[i];
             } else {
-                tempQueues[i] = new String[0];
+                tempQueues[i] = new String[0]; // Dummy placeholder queue if the original is full
             }
         }
 
@@ -428,12 +439,12 @@ public class Main {
         int cashierNumber = intInputPrompt("Enter cashier number: ", 0, cashiers.length - 1);
         String customerName = validatedInputPrompt("Enter name of customer: ");
 
-        if (addToQueue(cashiers[cashierNumber], customerName)) {
-            System.out.println("Successfully added customer to queue!");
+        if (!addToQueue(cashiers[cashierNumber], customerName)) { // Check if op failed
+            System.out.println("Couldn't add customer to queue! (Selected queue is full!)");
             return;
         }
 
-        System.out.println("Couldn't add customer to queue! (Selected queue is full!)");
+        System.out.printf("Successfully added customer %s to queue %d!\n", customerName, cashierNumber);
     }
 
     /**
@@ -446,12 +457,12 @@ public class Main {
         int positionIndex = intInputPrompt("Enter customer position: ", 0, cashiers[cashierNumber].length - 1);
         String customerName = popFromQueue(cashiers[cashierNumber], positionIndex);
 
-        if (customerName == null) {
+        if (customerName == null) { // Check if op failed
             System.out.println("No customer found in that position!");
             return;
         }
 
-        System.out.printf("Successfully removed customer %s from queue!\n", customerName);
+        System.out.printf("Successfully removed customer %s from queue %d!\n", customerName, cashierNumber);
     }
 
     /**
@@ -463,7 +474,7 @@ public class Main {
     private static void removeServedCustomer() {
         int newStock = burgerStock - burgersPerCustomer;
 
-        if (newStock < 0) {
+        if (newStock < 0) { // Ensure stock will not be negative
             System.out.println("Not enough items in stock! Cannot serve customer!");
             return;
         }
@@ -471,16 +482,16 @@ public class Main {
         int cashierNumber = intInputPrompt("Enter cashier number: ", 0, cashiers.length - 1);
         String customerName = popFromQueue(cashiers[cashierNumber], 0);
 
-        if (customerName == null) {
+        if (customerName == null) { // Ensure that customer is present in queue
             System.out.println("No customer found at the cashier!");
             return;
         }
 
-        if (newStock <= lowBurgerStock) {
+        if (newStock <= lowBurgerStock) { // Low stock warning
             System.out.println("Warning: Stock is running low!");
         }
 
-        burgerStock = newStock;
+        burgerStock = newStock; // Update stock if no validation failures occur
         System.out.printf("Successfully served customer %s!\n", customerName);
     }
 
@@ -503,6 +514,7 @@ public class Main {
      * Implements the SPD/106 option for the program.
      * This method stores data from {@link Main#burgerStock} and {@link Main#cashiers}
      * into a file in the current directory.
+     * Data is stored as text in a CSV format.
      */
     private static void storeProgramData() {
         try {
@@ -520,6 +532,7 @@ public class Main {
             System.out.println("Successfully stored data in file: " + defaultFileName);
 
             fileWriter.flush();
+            fileWriter.close();
         } catch (IOException error) {
             System.out.println("Could not create or read file! Data not stored!");
             System.out.println("Message is: " + error.getMessage());
@@ -529,7 +542,8 @@ public class Main {
     /**
      * Implements the LPD/107 option for the program.
      * This method loads data from a file in the current directory into {@link Main#burgerStock} and
-     * {@link Main#cashiers}
+     * {@link Main#cashiers}.
+     * The file must contain text in a CSV format in the right order.
      */
     private static void loadProgramData() {
         File file = new File(defaultFileName);
@@ -537,7 +551,7 @@ public class Main {
         try {
             Scanner fileReader = new Scanner(file);
 
-            if (!fileReader.hasNextInt()) {
+            if (!fileReader.hasNextInt()) { // Ensure stock value is available
                 System.out.println("Stock data is not available! Save data may be corrupted!");
                 return;
             }
@@ -546,7 +560,7 @@ public class Main {
             fileReader.nextLine();
 
             for (String[] queue : cashiers) {
-                if (!fileReader.hasNextLine()) {
+                if (!fileReader.hasNextLine()) { // Ensure all queues are available in file
                     System.out.println("Complete data is not available in save file!");
                     System.out.println("Data may have been partially loaded!");
                     burgerStock = savedStock;
@@ -555,14 +569,14 @@ public class Main {
 
                 String[] dataLine = fileReader.nextLine().split(",");
 
-                if (dataLine.length < queue.length) {
+                if (dataLine.length < queue.length) { // Ensure enough data is available for each queue
                     System.out.println("Complete data is not available in save file!");
                     System.out.println("Data may have been partially loaded!");
                     burgerStock = savedStock;
                     return;
                 }
 
-                for (int i = 0; i < queue.length; i++) {
+                for (int i = 0; i < queue.length; i++) { // Parse data and store in program memory
                     if (dataLine[i].equals("null")) {
                         queue[i] = null;
                     } else {
